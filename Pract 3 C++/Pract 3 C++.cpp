918dd4438e661a4a7d907ca863f5437425858811
Sasha <99606517+denly1@users.noreply.github.com>
2024-02-19 20:44:18 +0300

    Добавьте файлы проекта.

---
+++ Pract 3 C++/Pract 3 C++.cpp
@@ -0,0 +1,39 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    setlocale(0, "");
+
+    const int a = 3;
+    const int b = 5;
+    double array[a][b];
+    double sum, average;
+
+   
+    for (int q = 0; q < a; q++) {
+        cout << "Введите числа: ";
+        for (int r = 0; r < b; r++) {
+            cin >> array[q][r];
+        }
+    }
+
+    
+    for (int i = 0; i < a; i++) {
+        sum = 0;
+        for (int j = 0; j < b; j++) {
+            sum += array[i][j];
+        }
+        average = sum / 5;
+        cout << "Среднее арифметическое: " << average << endl;
+    }
+
+    return 0;
+}
